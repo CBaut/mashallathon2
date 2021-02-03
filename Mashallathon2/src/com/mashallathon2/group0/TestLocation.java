491bb97ed67f2dfae85ba9fb5c6b12d4a9868292
Chris Bautista <CBaut@users.noreply.github.com>
2021-02-03 00:03:45 -0800

    comments and test

--- Mashallathon2/src/com/mashallathon2/group0/TestLocation.java
+++ Mashallathon2/src/com/mashallathon2/group0/TestLocation.java
@@ -6,14 +6,19 @@ import com.mashallathon2.organism.Organism;
 
 public class TestLocation {
     public static void main(String[] args) {
+
+        // 1 locaton per group
+        // 1 organism per person
+
         Cat cat = new Cat("Meownica", 100, 100);
         ArrayList<Organism> housePopulation = new ArrayList<>();
         housePopulation.add(cat);
         MyHouse house = new MyHouse("Chris Casa", 1, housePopulation);
-
         cat.getName();
         cat.getHealth();
         cat.eat();
+        cat.move();
+        cat.sleep();
 
         house.getLocationName();
         house.earthquake();
